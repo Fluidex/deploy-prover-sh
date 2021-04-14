6e2a8b736844cbe65370cbacb805db2d88837c51
HAOYUatHZ <haoyu@protonmail.com>
2021-04-14 14:35:35 +0800

    minor

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 apt-get update
-# apt-get install -y --no-install-recommends \
-#     build-essential \
-#     ca-certificates \
-#     curl \
-#     git \
-#     ssh \
-#     libssl-dev \
-#     apt-utils \
-#     pkg-config \
-#     nasm \
-#     nlohmann-json3-dev
-# apt-get clean
-# rm -rf /var/lib/apt/lists/*
-# apt-get update
+apt-get install -y --no-install-recommends \
+    build-essential \
+    ca-certificates \
+    curl \
+    git \
+    ssh \
+    libssl-dev \
+    apt-utils \
+    pkg-config \
+    nasm \
+    nlohmann-json3-dev
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+apt-get update
 
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
